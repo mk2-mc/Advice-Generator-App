614bda59123bff141885391382e09344c6ffa25d
Mk.2 <55669745+mk2-mc@users.noreply.github.com>
2023-09-15 11:34:03 +0100

    Initial commit

---
+++ script.js
@@ -0,0 +1,19 @@
+const id = document.querySelector(".advice-id");
+const quote = document.querySelector(".advice-quote");
+const button = document.querySelector(".button");
+
+function getQuote() {
+  const url = "https://api.adviceslip.com/advice";
+  var timestamp = new Date().getTime();
+  
+  fetch(`${url}?ts=${timestamp}`)
+    .then((response) => {
+      return response.json();
+    })
+    .then((response) => {
+      id.innerHTML = `Advice #${response.slip.id}`;
+      quote.innerHTML = `${response.slip.advice}`;
+    });
+}
+
+button.addEventListener("click", getQuote);
